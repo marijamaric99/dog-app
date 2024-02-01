cb7bb4fbfebd83a043ff1cdc2d8d7256a4720057
marijamaric99 <marija.mari350@gmail.com>
2024-02-01 03:52:31 +0100

    Add icon

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -5,19 +5,19 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header
-      className={`bg-opacity-0 text-white p-2 flex justify-between items-center shadow-sm`}
+      className={"bg-opacity-0 text-white p-2 flex justify-between items-center shadow-sm"}
     >
       <Link legacyBehavior href="/">
         <a className="flex items-center">
           <Image
             src={logoImg}
             alt="A few dogs"
-            className={`ml-2 text-xl`}
+            className={"ml-2 text-xl"}
             width={400}
             height={400}
           />
           <span className="ml-6 text-2xl text-black text-center font-bold">
-            CEO's Dogs API
+            CEO Dogs API
           </span>
         </a>
       </Link>
